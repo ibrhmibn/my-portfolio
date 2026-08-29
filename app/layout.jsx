873237d6ef96be17b5ea380876e965c26e2e9c1c
ibrhmibn <ibrahimtyo39@gmail.com>
2026-08-29 21:13:40 +0700

    nambahne analytics vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { ThemeProvider } from "../components/ThemeProvider";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Ibrahim Portfolio",
